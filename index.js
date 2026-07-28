793c0e3ea32c30a28b9245690fc2e78e7f12b252
sousar4@hotmail.com <sousar4@hotmail.com>
2026-07-27 22:50:44 -0300

    iniciando o código para o terceiro desafio Felipão DIO

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Olá mundo!")
+class heroi {
+    constructor(nome, idade, poder) {
+        this.name = Zev
+        this.idade = 36
+        this.tipo = Guerreiro
+        this.poder = Espada
+    }
+    atacar(){
+        console.log("O " + `${this.tipo} + `${this.name} + atacou com ${this.poder}!`);
+            
+    }
+}
+console.log(heroi)
