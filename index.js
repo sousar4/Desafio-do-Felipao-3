8a373cf37c10626e982787eb0020b3ab3b3e0f5d
sousar4@hotmail.com <sousar4@hotmail.com>
2026-08-02 12:50:00 -0300

    criando meu heroi

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 //escrevendo modelo para DIO, posteriormente transformando para vscode
+const prompt = require('prompt-sync')();
+//- Variáveis
+let name = "Guerreiro Zev"
+console.log("Bem vindo ao jogo! Eu sou o " + name + " e vamos juntos vencer o desafio do Felipão!");
 
 class Heroi {
-    constructor(nome, idade, poder) {
+    constructor(name, idade, tipo) {
         this.name = "Zev"
         this.idade = 36
         this.tipo = Guerreiro
     }
     atacar(){
-        console.log("O " + `${this.tipo} + `${this.name} + atacou com ${this.poder}!`);
+        console.log("O " + `${this.tipo}` + `${this.name}` + atacou com `${this.poder}!`);
             
     }
 }
