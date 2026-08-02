d1bf34b7ac6cdc7a8e8aa3fa6bacaa365a9357e9
sousar4@hotmail.com <sousar4@hotmail.com>
2026-08-02 12:35:10 -0300

    copilando código para DIO posterior transformar para vscode - js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-class heroi {
+//escrevendo modelo para DIO, posteriormente transformando para vscode
+
+class Heroi {
     constructor(nome, idade, poder) {
-        this.name = Zev
+        this.name = "Zev"
         this.idade = 36
         this.tipo = Guerreiro
-        this.poder = Espada
     }
     atacar(){
         console.log("O " + `${this.tipo} + `${this.name} + atacou com ${this.poder}!`);
